2db5115fd3489f8e4535cdd23636db6aa9b2a6f6
Peter Kuchel <petermichaelkuchel@gmail.com>
2023-04-02 04:55:54 +0000

    python files containing the tables for the db, as well as code that creates them from python and connecting to the db, assumes that a postgres server is already set up and is configured correctly

---
+++ server/create_tables.py
@@ -0,0 +1,41 @@
+import psycopg2 
+import postgres_tables
+from dsn import DSN
+
+
+# resoruces 
+# https://www.psycopg.org/docs/connection.html
+
+
+def drop_all_tables(conn):
+    curs = conn.cursor()
+    
+    print("dropping tables first")
+    
+    drop_query = "DROP TABLE IF EXISTS " + ", ".join(postgres_tables.TABLE_NAMES) + ";"
+    
+    print("dropping tables successful")
+    
+    curs.execute(drop_query)
+    conn.commit()
+
+def create_postgres_tables(conn):
+    
+    curs = conn.cursor()
+    for (i, table_query) in enumerate(postgres_tables.TABLES_CREATE_QUERIES):
+        
+        print(f"table {i} from the list to be executed")
+        
+        curs.execute(table_query)
+        
+        print(f"table {i} from the list has been successfully made")
+        
+        conn.commit()
+
+if __name__ == "__main__":
+    conn = psycopg2.connect(DSN)
+    create_postgres_tables(conn)
+    conn.close()
+    
+            
+    
