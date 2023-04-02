e0e56b8bd51bba20a2e3e1f2ceddef9cff4e17b4
Peter Kuchel <petermichaelkuchel@gmail.com>
2023-04-02 23:57:56 +0000

    added to functions to geta sample of the scraped data

--- webscraper/main.py
+++ webscraper/main.py
@@ -1,6 +1,16 @@
 import vehicles
 import motorcycles
 import pandas as pd
+import pickle
+
+
+def scrape_vehicles(num_of_pages:int):
+    vehicles_df = vehicles.scrape_vehicles(num_of_pages)
+    return vehicles_df 
+
+def scrape_motorcycles(num_of_pages:int):
+    motorcycles_df = motorcycles.scrape_motorcycles(num_of_pages)
+    return motorcycles_df
 
 def scrape():
     vehicles_df = vehicles.scrape_vehicles(1)
@@ -19,3 +29,22 @@ def insert_user_data():
 
 def notify_user():
     print("Email sent to user")
+    
+    
+def save_sample_vehicle():
+    df = scrape_vehicles(1) 
+    to_pickle = [dict(row) for _, row in df.iterrows()]
+    with open('vehicle_sample.pickle', 'wb') as f:
+        pickle.dump(to_pickle, f)
+        
+def save_sample_motorcycle():
+    df = scrape_motorcycles(1) 
+    to_pickle = [dict(row) for _, row in df.iterrows()]
+    with open('motorcycle_sample.pickle', 'wb') as f:
+        pickle.dump(to_pickle, f)
+        
+        
+        
+if __name__ == "__main__":
+    save_sample_vehicle()
+    save_sample_motorcycle()
