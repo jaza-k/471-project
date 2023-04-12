c963771bb2318d2268eb3a278567d60002daa14a
Peter Kuchel <petermichaelkuchel@gmail.com>
2023-04-12 18:08:46 +0000

    added the functionality where the user will be emailed when a possible match has been found

--- server/dbfunctions/dbfuncs.py
+++ server/dbfunctions/dbfuncs.py
@@ -1,5 +1,6 @@
 import psycopg2
 from datetime import datetime
+from emails import send_email
 
 def search_matches_found(curs, conn, _matches, usr_search_uuid):
 
@@ -24,6 +25,27 @@ def search_matches_found(curs, conn, _matches, usr_search_uuid):
             INSERT INTO matches_with (_search_id, _matched_ad_id)
             VALUES ('{usr_search_uuid}', '{m[0]}')"""
             curs.execute(insert_matches_q)
+            
+            # send the user an email 
+            get_usr = """
+            "SELECT 
+                us.email, 
+                ur.name 
+            FROM 
+                user_search as us, 
+                _user as ur 
+            WHERE 
+                us._sid = %s AND
+                us._email = ur.email; 
+            """
+            
+            curs.execute(get_usr, (usr_search_uuid,)) 
+            res = curs.fetchone()
+            email, fname = res 
+            
+            send_email.send_email_notification(email, fname) 
+            
+            
             
 
 def check_matches_against_user_searches(conn):
